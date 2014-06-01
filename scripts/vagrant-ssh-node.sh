bb589b1fe641981a4d8254de817551ebfa06faef
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2014-06-02 00:27:08 +0200

    fix: vagrant-ssh-node.sh not working on Windows

--- scripts/vagrant-ssh-node.sh
+++ scripts/vagrant-ssh-node.sh
@@ -4,6 +4,6 @@
 # or IDE in the host operating system. Don't know why, but Vagrantfile port forwarding does not work.
 # (https://groups.google.com/forum/#!topic/vagrant-up/RzPooJ0dp6Q)
 
-/usr/bin/vagrant ssh-config > $TMPDIR/vagrant-ssh-config
+vagrant ssh-config > $TMPDIR/vagrant-ssh-config
 ssh -F $TMPDIR/vagrant-ssh-config -L 5858:127.0.0.1:5858 default
 rm $TMPDIR/vagrant-ssh-config
